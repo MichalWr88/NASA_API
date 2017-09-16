148de88578c059e00f2372431456af0bd4d6e607
MIchal Maleszewski <maleszewski.geo@gmail.com>
2017-09-16 09:50:26 +0200

    update gallery

--- app/js/script.js
+++ app/js/script.js
@@ -1,37 +1,140 @@
-$(document).ready(function(){
-
-let config ={
-	url:'https://api.nasa.gov/planetary/apod20170505api_key=pzXOn2yp335zmTjQ2lqolN8T5nFXtMUXXnr9hZc7',
-	// url:'https://images-api.nasa.gov/search?q=()&media_type=image',
-	type: 'GET',
-	
-			
-};
-
-let header = $('.page__header');
-	
-
-
+$(document).ready(function() {
+    // opening elements
+    let opening = $('#opening1'),
+        picture = $('#opPicture1'),
+        description = $('.opening__description'),
+        autor = $('#autor'),
+        title = $('#title'),
+        figure = $('.picture__figure'),
+        startBtn = $('.opening__start');
+    // ---------------------------------
+    let gallery = $('.gallery__list'),
+        loadBtn = $('.gallery__load');
 
 
 
-   $.ajax(config)
-   .done(function(data) {
-   	console.log("success");
-   	console.log(data);
-   	// header.css('background-image', 'url("' + data.collection.items[0].href + '")');
+loadBtn.on('click', function(event) {
+    event.preventDefault();
+    /* Act on the event */
+    let galleryElem = $('<li>',{
+            class: 'gallery__Elem'
+        }),
+         dataRand = formatDate(randomDate());
+         
 
-   })
-   .fail(function(err) {
-   	console.log("error");
-   })
-   .always(function() {
-   	console.log("complete");
-   });
-   
+    loadPicture(dataRand)
+    .done(function(data) {
+            console.log("success");
+            let imgElem = $('<img>',{
+                class:'elem__img',
+                src: data.url,
+                timeout: 5000
+            });
+            console.log(imgElem);
+            galleryElem.append(imgElem);
+            console.log(data);
+        })
+        .fail(function(err) {
+            console.log('error');
+        })
+        .always(function() {
+            console.log("complete");
+        });
+            
+        gallery.append(galleryElem);
+}); 
+    // for (let i = 0; i < 1; i++) {
+    //     let galleryElem = $('<li>',{
+    //         class: 'gallery__Elem'
+    //     }),
+    //      dataRand = formatDate(randomDate());
+         
+
+    // loadPicture(dataRand)
+    // .done(function(data) {
+    //         console.log("success");
+    //         let imgElem = $('<img>',{
+    //             class:'elem__img',
+    //             src: data.url
+    //         });
+    //         console.log(imgElem);
+    //         galleryElem.append(imgElem);
+    //         console.log(data);
+    //     })
+    //     .fail(function(err) {
+    //         console.log('error');
+    //     })
+    //     .always(function() {
+    //         console.log("complete");
+    //     });
+            
+    //     gallery.append(galleryElem);
+    // }
+
+
+    startBtn.on('click', function(event) {
+        event.preventDefault();
+        /* Act on the event */
+        $(this).slideUp(500);
+    });
+
+    // initBG(picture,figure,title,autor);
+
 
 });
+/*-------------------------------------------*/
+let loadPicture = (obj) => {
+    let config = {
+        url: `https://api.nasa.gov/planetary/apod?date=${obj}&api_key=pzXOn2yp335zmTjQ2lqolN8T5nFXtMUXXnr9hZc7`,
+        type: 'GET'
+    };
 
-let initBG = (elem)=>{
-
+    return $.ajax(config)
 };
+
+let initBG = function(header, desc, title, autor) {
+
+    let dataRand = formatDate(randomDate());
+
+    loadPicture(dataRand)
+    .done(function(data) {
+            console.log("success");
+            console.log(data);
+            header.css('background-image', 'url("' + data.url + '")');
+            desc.text(`${data.explanation}`);
+            title.text(`${data.title}`);
+            autor.text(`${data.copyright} `);
+        })
+        .fail(function(err) {
+            console.log(err);
+        })
+        .always(function() {
+            console.log("complete");
+        });
+   
+};
+
+
+let randomDate = () => {
+    let startDate = new Date(2010, 0, 1).getTime();
+    let endDate = new Date(2017, 0, 1).getTime();
+    let spaces = (endDate - startDate);
+    let timestamp = Math.round(Math.random() * spaces);
+    timestamp += startDate;
+    return new Date(timestamp);
+}
+let formatDate = (date) => {
+    let month = randomDate().getMonth() + 1;
+    let day = randomDate().getDate();
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+    return String(date.getFullYear()) + '-' + month + '-' + day;
+}
+
+
+
+
+
+// $( window ).load(function() {
+//   // Run code
+// });
